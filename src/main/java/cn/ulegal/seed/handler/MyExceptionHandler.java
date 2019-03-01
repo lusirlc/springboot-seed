3bb264b43c5927b28382d0d6b172945cce1a0135
luchuan <luchuan365@126.com>
2019-03-01 14:39:53 +0800

    添加参数校验全局处理器

--- src/main/java/cn/ulegal/seed/handler/MyExceptionHandler.java
+++ src/main/java/cn/ulegal/seed/handler/MyExceptionHandler.java
@@ -6,8 +6,16 @@ import cn.ulegal.seed.enums.BaseResultEnum;
 import cn.ulegal.seed.exception.BusinessException;
 import com.google.common.collect.ImmutableMap;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created By Chuan Lu On 2019/2/28 12:52
@@ -21,9 +29,13 @@ public class MyExceptionHandler {
 	//使用builder来构建一个异常类型和错误代码的异常
 	protected static ImmutableMap.Builder<Class<? extends Throwable>, BaseResultEnum> builder = ImmutableMap.builder();
 	
-	//捕获Exception异常
+	/**
+	 * 捕获Exception异常
+	 * @param e
+	 * @return
+	 */
 	@ExceptionHandler(Exception.class)
-	public BaseResult exception(Exception e) {
+	public BaseResult handleException(Exception e) {
 		log.error("catch exception:{}\r\nexception:{} ", e.getMessage(), e);
 		if (EXCEPTIONS == null)
 			EXCEPTIONS = builder.build();
@@ -39,8 +51,67 @@ public class MyExceptionHandler {
 		return BaseResult;
 	}
 	
+	/**
+	 * 捕获业务异常
+	 * @param exception
+	 * @return
+	 */
 	@ExceptionHandler(BusinessException.class)//可以直接写@ExceptionHandler,不指明异常类，会自动映射
-	public BaseResult CreditLoserExceptionHnadler(BusinessException exception) { //还可以声明接收其他任意参数
-		return BaseResultGenerator.error( BaseResultEnum.FAIL);
+	public BaseResult handleBusinessException(BusinessException exception) { //还可以声明接收其他任意参数
+		return BaseResultGenerator.error(exception.getErrorCode(),exception.getErrorMessage());
+	}
+	
+	/**
+	 * 字段校验
+	 * @param ex
+	 * @return
+	 */
+	@ExceptionHandler(ConstraintViolationException.class)
+	public BaseResult handleValidationException(ConstraintViolationException ex){
+		StringBuilder msgBuilder = new StringBuilder();
+		StringBuilder messageBuilder = new StringBuilder();
+		Set<ConstraintViolation<?>> constraintViolations = ex.getConstraintViolations();
+		if (!CollectionUtils.isEmpty(constraintViolations)) {
+			for(ConstraintViolation<?> s: constraintViolations){
+				messageBuilder.append(s.getPropertyPath()+": "+s.getMessage()+",");
+				msgBuilder.append(s.getMessage()+",");
+			}
+			String errorMessage = msgBuilder.toString();
+			String message = messageBuilder.toString();
+			if (errorMessage.length() > 1) {
+				errorMessage = errorMessage.substring(0, errorMessage.length() - 1);
+			}
+			if (message.length() > 1) {
+				message = message.substring(0, message.length() - 1);
+			}
+			log.info("请求参数不合法 -> {}", message);
+			return BaseResultGenerator.error(errorMessage);
+		}
+		return BaseResultGenerator.error();
+	}
+	
+	/**
+	 * Bean校验
+	 * @param ex
+	 * @return
+	 */
+	@ExceptionHandler(BindException.class)
+	public BaseResult handleBindException(BindException ex) {
+		String errorMessage = null;
+		List<ObjectError> objectErrors = ex.getBindingResult().getAllErrors();
+		if (!CollectionUtils.isEmpty(objectErrors)) {
+			StringBuilder msgBuilder = new StringBuilder();
+			String paramName = objectErrors.get(0).getObjectName();
+			for (ObjectError objectError : objectErrors) {
+				msgBuilder.append(objectError.getDefaultMessage()).append(",");
+			}
+			errorMessage = msgBuilder.toString();
+			if (errorMessage.length() > 1) {
+				errorMessage = errorMessage.substring(0, errorMessage.length() - 1);
+			}
+			log.info("请求参数不合法 -> {}",paramName + ":" + errorMessage);
+			return BaseResultGenerator.error(errorMessage);
+		}
+		return BaseResultGenerator.error();
 	}
 }
